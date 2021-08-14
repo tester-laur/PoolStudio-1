f735fa3e2bcd87b673d3e2a71ca130575a23895c
michael vargas roa <michael.vr25@hotmail.com>
2021-08-14 17:12:19 -0400

    sección 1 y 2

---
+++ PoolStudio.WEB/Controllers/UsuariosController1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PoolStudio.WEB.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        //[HttpGet]
+        public IActionResult Index(int id)
+        {
+            ViewData["id"] = id;
+            return View();
+        }
+    }
+}
